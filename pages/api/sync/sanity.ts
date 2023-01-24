bb0b11baec36cb7744ec2df5b26098b301b908a1
Evangelos Pallis <vpallis@spirit.com.gr>
2023-01-24 14:32:10 +0200

    proof of concept

--- pages/api/sync/sanity.ts
+++ pages/api/sync/sanity.ts
@@ -1,8 +1,8 @@
-import sanityClient from '@sanity/client'
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { nanoid } from 'nanoid'
 import OpenTypeAPI from '../../../lib/api/OpenTypeAPI.js'
 import Sanity from '../../../lib/api/Sanity'
+import slugify from 'slugify'
 
 // writing to sanity client
 // what's the incoming data?
@@ -12,11 +12,10 @@ export default async function handler(
   res: NextApiResponse
 ) {
 
-  // await OpenTypeAPI.getInstance(context)
 
   const sanity = new Sanity()
 
-  console.log('Webhook payload:', req.body);
+  console.log('Webhook payload:', req.body)
 
   // Initiate Sanity transaction to perform the following chained mutations
   let sanityTransaction = sanity.transaction()
@@ -32,9 +31,27 @@ export default async function handler(
 
   // Define product fields
   const productFields = {
-    name,
+    name
     // UUID,
     // etc.
+  }
+  const OpenType = await OpenTypeAPI.getInstance()
+  console.log(await OpenType.getFonts())
+  for (const font of await OpenType.getFonts()) {
+    const dataObject = {
+      _id: nanoid(),
+      _type: 'product',
+      name: font.familyName,
+      slug: { current: slugify(font.familyName) },
+      uid: font.getUid(),
+      version: font.version
+    }
+    const product = await sanity.findByUidAndVersion(font.getUid(), font.version)
+    if (product) {
+      dataObject['_id'] = product['_id']
+    }
+    sanity.createOrReplace(dataObject)
+    break
   }
 
   /*  ------------------------------ */
@@ -43,18 +60,18 @@ export default async function handler(
 
   console.log('Writing product to Sanity...')
 
-  sanityTransaction = sanityTransaction.createIfNotExists(product)
+  // sanityTransaction = sanityTransaction.createIfNotExists(product)
+  //
+  // // Patch (update) product document with core commerce data
+  // sanityTransaction = sanityTransaction.patch(_id, (patch) => patch.set(productFields))
+  //
+  // const result = await sanityTransaction.commit();
 
-  // Patch (update) product document with core commerce data
-  sanityTransaction = sanityTransaction.patch(_id, (patch) => patch.set(productFields))
+  // console.info('Sync complete!');
+  // console.log('Result', result);
 
-  const result = await sanityTransaction.commit();
-
-  console.info('Sync complete!');
-  console.log('Result', result);
-
-  res.statusCode = 200;
-  res.json(JSON.stringify(result));
+  res.statusCode = 200
+  res.json({})
 
 }
 
