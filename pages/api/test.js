e24c3256e5893f35192055b26000647bd0dd48df
vpsnak <info.vpsnak@gmail.com>
2023-08-23 15:04:10 +0300

    finalize caching and create test api route to test sanityFetch

---
+++ pages/api/test.js
@@ -0,0 +1,10 @@
+import sanityFetch from '../../lib/utils/sanity.fetch'
+
+export default async function test(req, res) {
+  const test = await sanityFetch('*[_type == $type && uid == $uid][0]', {
+    type: 'font',
+    uid: 'Rather v2'
+  })
+  console.log('ortype', test.ortype)
+  res.end()
+}
