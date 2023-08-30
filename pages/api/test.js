3e8a4b73dfd76b68dad003ef673ba9514a2f4f52
Owen Hoskins <owen.hoskins@gmail.com>
2023-08-30 14:55:54 +0300

    Added width calculation methods to OrType class and include metafields in Sanity font/variant doc response

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,10 +1,23 @@
 import sanityFetch from '../../lib/utils/sanity.fetch'
+import { fontVariantsQuery } from '../../lib/sanity.queries'
 
 export default async function test(req, res) {
+  /*
+  const sanityFontVariants = await sanityFetch(fontVariantsQuery)
+  console.log(
+    'get width of word "ortype" at font-size 12px',
+    sanityFontVariants.map((variant) => {
+      return variant?.name + ': ' + variant?.ortype?.getWordWidth('ortype', 12)
+    })
+  )
+  */
+
+  /*
   const test = await sanityFetch('*[_type == $type && uid == $uid][0]', {
     type: 'font',
-    uid: 'Rather v2'
+    uid: 'Rather v2',
   })
   console.log('ortype', test._id)
+  */
   res.end()
 }
