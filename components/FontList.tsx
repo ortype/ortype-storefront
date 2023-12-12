91b7cfb0190fc04a66c2bb9814fac0ebca1295f7
Owen Hoskins <owen.hoskins@gmail.com>
2023-12-12 17:05:43 +0200

    Temp fix for broken font variant references

--- components/FontList.tsx
+++ components/FontList.tsx
@@ -8,16 +8,20 @@ export default function FontIndex({ fonts }: { fonts: Font[] }) {
         Fonts
       </h2>
       <div className="mb-32 grid grid-cols-1 gap-y-20 md:grid-cols-2 md:gap-x-16 md:gap-y-32 lg:gap-x-32">
-        {fonts.map((font, index) => (
-          <Tester
-            fontId={font._id}
-            variants={font.variants}
-            defaultVariantId={font.variants[0]._id}
-            index={index}
-            title={font.name}
-            slug={font.slug}
-          />
-        ))}
+        {fonts.map((font, index) =>
+          font.variants[0] ? (
+            <Tester
+              fontId={font._id}
+              variants={font.variants}
+              defaultVariantId={font.variants[0]._id}
+              index={index}
+              title={font.name}
+              slug={font.slug}
+            />
+          ) : (
+            <></>
+          )
+        )}
       </div>
     </section>
   )
