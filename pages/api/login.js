b06f281498e00082b1ee540865f03c2a6f83f40e
oryouormeorwe <info@ortype.is>
2025-01-15 13:27:28 +0200

    chore: clean deprecated api routes

--- pages/api/login.js
+++
@@ -1,8 +0,0 @@
-export default function loginAPI(req, res) {
-  /*  res.setHeader(
-    'Set-Cookie',
-    `authorizer-client=;Expires=-1;Secure=true;HttpOnly=true;Path=/`
-  )
-*/ console.log('logged in successfully')
-  res.status(200).json({ message: 'logged in successfully' })
-}
