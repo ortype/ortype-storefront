74209ac2b23290f335c49923963f883b53e5227d
oryouormeorwe <info@ortype.is>
2025-02-27 15:35:08 +0200

    chakra-theme ejected for gradual styling overrides

--- src/components/ui/provider.tsx
+++ src/components/ui/provider.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { system } from '@/@chakra-ui/theme'
+import { system } from '@/theme'
 import { ChakraProvider } from '@chakra-ui/react'
 import { ColorModeProvider, type ColorModeProviderProps } from './color-mode'
 
---
+++ src/theme/breakpoints.ts
@@ -0,0 +1,7 @@
+export const breakpoints = {
+  sm: '24em', // ~375px. em is a relative unit and is dependant on the font size.
+  md: '48em', // ~768px
+  lg: '64em', // ~1024px
+  xl: '77.5em', // ~1240px
+  '2xl': '100em', // ~1600px
+}
