55c24a3bc31ece4f9ac64bca0d9197e0ce5ec7ed
Owen Hoskins <owen.hoskins@gmail.com>
2026-08-20 13:12:01 +0300

    refactor(sanity): update disable draft mode button implementation

--- src/sanity/components/DisableDraftMode.tsx
+++ src/sanity/components/DisableDraftMode.tsx
@@ -1,46 +1,27 @@
-// src/components/DisableDraftMode.tsx
+// sanity/lib/components/DisableDraftMode.tsx
 
 'use client'
 
-import { useRouter } from 'next/navigation'
-import { useEffect, useState } from 'react'
+import { useIsPresentationTool } from 'next-sanity/hooks'
 
 export function DisableDraftMode() {
-  const [isPreview, setIsPreview] = useState(false)
-  const router = useRouter()
+  const isPresentationTool = useIsPresentationTool()
 
-  useEffect(() => {
-    // Check draft mode status via a simple fetch
-    const checkDraft = async () => {
-      const res = await fetch('/api/draft/status')
-      const data = await res.json()
-      setIsPreview(data.isEnabled)
-    }
-    checkDraft()
-  }, [])
-
-  const exitPreview = async () => {
-    await fetch('/api/draft-mode/disable', { method: 'POST' })
-    router.refresh()
-  }
-
-  if (!isPreview) return null
+  // Hide the button when inside the Presentation Tool
+  if (isPresentationTool) return null
 
   return (
     <a
-      // onClick={exitPreview}
       href="/api/draft-mode/disable"
       style={{
         position: 'fixed',
-        bottom: 0,
-        right: 0,
+        top: 0,
+        width: '100%',
         padding: '0.5rem',
-        margin: '1rem',
         background: '#eee',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        borderRadius: '10px',
       }}
     >
       Disable Draft Mode
